dbe4f66f1c8efb4848abed0cc8d1ff8ccb61cdba
yasamin nazari <yasminnazari2009@gmail.com>
2025-09-22 16:09:38 +0330

    sim_card.py

--- sim_card.py
+++ sim_card.py
@@ -1,5 +1,5 @@
 class SimCard:
-    def __init__(self, phone_number, company, ram):
+    def __init__(self, phone_number, company, storage):
         self.phone_number = phone_number
         self.company = company
-        self.ram = ram
+        self.storage = storage
